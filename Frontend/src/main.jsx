4fa494d05d9ee1fa09db3e49d5bd837008477980
Ayush Behera <himanishayush2003@gmail.com>
2026-06-08 19:02:51 +0530

    Corrected the css of register and login page

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,10 @@
-
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "../../../bootstrap-5.3.8-dist/bootstrap-5.3.8-dist/css/bootstrap.min.css"
+import "../../../bootstrap-5.3.8-dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-      <App />
+    <App />
   </BrowserRouter>,
 );
-
