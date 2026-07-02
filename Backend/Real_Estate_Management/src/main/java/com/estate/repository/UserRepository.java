aa141614c73cf7fd6d3e3a46b565bd3cc0dc95cb
Ayush Behera <himanishayush2003@gmail.com>
2026-07-02 22:01:45 +0530

    made some changes in userRepository

--- Backend/Real_Estate_Management/src/main/java/com/estate/repository/UserRepository.java
+++ Backend/Real_Estate_Management/src/main/java/com/estate/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.estate.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.estate.entities.User;
+import com.estate.entities.UserRole;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	User findByEmailAndPassword(String email, String password);
+
+	List<User> findByUserRoles(UserRole userRoles);
 }
