09ec9f0a947061793f73c939dfbfb4d3b8be24d4
atharvdadhe07 <atharvdadhe@gmail.com>
2026-07-20 17:16:09 +0530

    added get property

--- Backend/Real_Estate_Management/src/main/java/com/estate/controllers/PropertyController.java
+++ Backend/Real_Estate_Management/src/main/java/com/estate/controllers/PropertyController.java
@@ -1,13 +1,18 @@
 package com.estate.controllers;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.estate.dtos.PropertyRequestDTO;
+import com.estate.dtos.PropertyResponseDTO;
 import com.estate.entities.Property;
 import com.estate.service.PropertyService;
 
@@ -19,8 +24,37 @@ import lombok.RequiredArgsConstructor;
 public class PropertyController {
 	private final PropertyService propertyService;
 	
+	//----------------------------Create Property--------------------------------------------//
 	@PostMapping
 	public ResponseEntity<Property> addProperty(@RequestBody PropertyRequestDTO dto){
 		return new ResponseEntity<>(propertyService.addProperty(dto),HttpStatus.CREATED);
 	}
+	
+	@GetMapping
+	public ResponseEntity<List<PropertyResponseDTO>> getAllProperty() {
+
+	    return ResponseEntity.ok(propertyService.listAllProperty());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<PropertyResponseDTO> getPropertyById(@PathVariable Long id){
+		return ResponseEntity.ok(propertyService.getProperty(id));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
