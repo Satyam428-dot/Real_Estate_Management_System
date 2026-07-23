2ea3a930dca64fb769522b5106b13101d7fa392f
atharvdadhe07 <atharvdadhe@gmail.com>
2026-07-23 14:11:50 +0530

    Created ownerPage

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -15,6 +15,11 @@ import ApproveProperties from "./pages/admin/ApproveProperties";
 import ManageListings from "./pages/admin/ManageListing";
 import ViewReports from "./pages/admin/ViewReport";
 
+import BuyerDashboard from "./pages/buyer/BuyerDashboard";
+
+import OwnerDashboard from "./pages/owner/OwnerDashboard";
+import OwnerOverview from "./pages/owner/OwnerOverview";
+
 function App() {
   return (
     <>
@@ -38,6 +43,16 @@ function App() {
           <Route path="listings" element={<ManageListings />} />
           <Route path="reports" element={<ViewReports />} />
         </Route>
+
+         {/* Buyer Dashboard */}
+          <Route path="/buyer" element={<BuyerDashboard />} />
+
+        {/* Owner Routes */}
+        <Route path="/owner" element={<OwnerDashboard />}>
+          <Route index element={<OwnerOverview />} />
+          <Route path="dashboard" element={<OwnerOverview />} />
+        </Route>
+          
       </Routes>
     </>
   );
---
+++ Frontend/src/pages/owner/OwnerOverview.jsx
@@ -0,0 +1,8 @@
+export default function OwnerOverview() {
+  return (
+    <div>
+      <h1>Welcome back, John Owner 👋</h1>
+      <p>Dashboard content will go here.</p>
+    </div>
+  );
+}
