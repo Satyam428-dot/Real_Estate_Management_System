11851738ac9b02dcab0c69ba824b44eb2645d570
AbhishekDhoran33 <abhishekdhoran33@gmail.com>
2026-07-26 11:47:53 +0530

    Created the Scheduled Properties page for the buyer.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -18,7 +18,7 @@ import BuyerDashboard from "./pages/buyer/BuyerDashboard";
 import BuyerOverview from "./pages/buyer/BuyerOverview"; // 👈 ADD THIS IMPORT LINE HERE
 import BrowseProperties from "./pages/buyer/BrowseProperties";
 import SavedProperties from "./pages/buyer/SavedProperties";
-// import ScheduledVisits from "./pages/buyer/ScheduledVisits";
+import ScheduledVisits from "./pages/buyer/ScheduledVisits";
 
 function App() {
   return (
@@ -45,7 +45,7 @@ function App() {
         <Route path="dashboard" element={<BuyerOverview />} />
         <Route path="browse" element={<BrowseProperties />} />
         <Route path="saved" element={<SavedProperties />} />
-        {/* <Route path="visits" element={<ScheduledVisits />} /> */}
+        <Route path="visits" element={<ScheduledVisits />} />
       </Route>
     </Routes>
   );
