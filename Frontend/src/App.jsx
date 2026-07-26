0268ebc37bf6cdf2c19e81ce6ed2852f5412c63d
AbhishekDhoran33 <abhishekdhoran33@gmail.com>
2026-07-26 12:21:46 +0530

    Created the My Bookings page for the buyer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -19,6 +19,7 @@ import BuyerOverview from "./pages/buyer/BuyerOverview"; // 👈 ADD THIS IMPORT
 import BrowseProperties from "./pages/buyer/BrowseProperties";
 import SavedProperties from "./pages/buyer/SavedProperties";
 import ScheduledVisits from "./pages/buyer/ScheduledVisits";
+import MyBookings from "./pages/buyer/MyBookings";
 
 function App() {
   return (
@@ -46,6 +47,7 @@ function App() {
         <Route path="browse" element={<BrowseProperties />} />
         <Route path="saved" element={<SavedProperties />} />
         <Route path="visits" element={<ScheduledVisits />} />
+        <Route path="bookings" element={<MyBookings />} />
       </Route>
     </Routes>
   );
